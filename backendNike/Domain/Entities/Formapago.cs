e6c3a084309c4f4b122752d58a0095bf859797a4
zzfuture <113058239+zzfuture@users.noreply.github.com>
2023-12-18 21:36:09 -0500

    dd

--- backendNike/Domain/Entities/Formapago.cs
+++ backendNike/Domain/Entities/Formapago.cs
@@ -1,11 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace Domain.Entities;
-
-public partial class Formapago : BaseEntity
-{
-    public int Id { get; set; }
-
-    public string? Nombre { get; set; }
-}
+﻿
